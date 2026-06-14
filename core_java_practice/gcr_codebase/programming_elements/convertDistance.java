258b5fcbcd42cdcf0a41bf841cd142a0189ad141
RaghavAg0522 <raghav.agrawal5509@gmail.com>
2026-06-15 02:30:40 +0530

    Add files via upload
    Added-(ASSIGMENT-01, Programing Elements Submissions)

---
+++ core_java_practice/gcr_codebase/programming_elements/convertDistance.java
@@ -0,0 +1,7 @@
+public class convertDistance {
+    static void main(String[] args) {
+        double distance = 10.8;
+        double Miles = distance/1.6;
+        System.out.println(" The distance " + distance +"km in miles is "+Miles);
+    }
+}
