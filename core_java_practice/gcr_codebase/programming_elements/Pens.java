aa755d98e1f672eecb9887402d9964d89b004f46
RaghavAg0522 <raghav.agrawal5509@gmail.com>
2026-06-23 13:09:25 +0530

    Delete core_java_practice/gcr_codebase directory

--- core_java_practice/gcr_codebase/programming_elements/Pens.java
+++
@@ -1,9 +0,0 @@
-public class Pens {
-    static void main(String[] args) {
-        int pens =14;
-        int students = 3;
-        double M_pens = 14/3;
-        int remaining_pens=14%3;
-        System.out.println("The Pen Per Student is "+M_pens+ " and the remaining pen not distributed is "+remaining_pens);
-    }
-}
